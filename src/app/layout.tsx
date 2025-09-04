5af9340d7776fe52dd74f61ecda8a32837bfbca8
Jose Moraes <elvano@gmail.com>
2025-09-04 23:05:36 +0000

    Try fixing this error: `Console Error: In HTML, <html> cannot be a child

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
 import { ReactNode } from 'react';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
+import './globals.css';
 
 type Props = {
   children: ReactNode;
+  params: { locale: string };
 };
 
 export default async function RootLayout({
-  children
+  children,
+  params: { locale }
 }: Props) {
-  const locale = await getLocale();
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
-      <body>
+    <html lang={locale} suppressHydrationWarning>
+       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased">
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
