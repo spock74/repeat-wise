e0406901460bedc8e8743a9f497d187a211446aa
Jose Moraes <elvano@gmail.com>
2025-09-09 20:29:16 +0000

    Initial, but second

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,4 +3,11 @@ import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
 export const locales = ['en', 'pt'] as const;
 export const localePrefix = 'always'; // Default
  
-export const {Link, redirect, usePathname, useRouter} = createLocalizedPathnamesNavigation({locales, localePrefix});
+export const pathnames = {
+  '/dashboard': {
+    en: '/dashboard',
+    pt: '/painel'
+  }
+} as const;
+
+export const {Link, redirect, usePathname, useRouter} = createLocalizedPathnamesNavigation({locales, localePrefix, pathnames});
