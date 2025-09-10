7bb4ef65908302e7414c0919375b15efbc031a38
Jose Moraes <elvano@gmail.com>
2025-09-10 00:03:26 +0000

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,34 @@
 import createMiddleware from 'next-intl/middleware';
-import {locales, pathnames} from './navigation';
  
 export default createMiddleware({
-  defaultLocale: 'en',
-  locales,
-  pathnames
+  // A list of all locales that are supported
+  locales: ['en', 'pt'],
+ 
+  // Used when no locale matches
+  defaultLocale: 'pt',
+  localePrefix: 'always',
+  pathnames: {
+    '/': '/',
+    '/dashboard': {
+      en: '/dashboard',
+      pt: '/painel'
+    },
+    '/generate': {
+      en: '/generate',
+      pt: '/gerar'
+    },
+    '/manage-sets': {
+      en: '/manage-sets',
+      pt: '/gerir-conjuntos'
+    },
+    '/settings': {
+      en: '/settings',
+      pt: '/configuracoes'
+    }
+  }
 });
  
 export const config = {
+  // Match only internationalized pathnames
   matcher: ['/', '/(pt|en)/:path*']
 };
